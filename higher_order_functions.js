3e9a31c377eb633405a9c568396638cbc8749756
Thirumala2004 <thirumalanaiduponnaganti@gmail.com>
2024-09-28 11:21:40 +0530

    Add files via upload

---
+++ higher_order_functions.js
@@ -0,0 +1,25 @@
+/*
+
+const randNumbers=[4,11,42,14,39]
+const filteredArray=randNumbers.filter(n=>{return n>15;});
+console.log(filteredArray);
+
+*/
+
+/*
+
+const initialValue=0;
+const array1=[1,2,3,4]
+const sumWithInitial=array1.reduce((accumulator,currentValue)=>accumulator+currentValue,initialValue);
+console.log(sumWithInitial)
+
+*/
+
+/*
+
+
+const finalParticipants=['Taylor','Donald','Don','Natasha','Bobby'];
+const announcements=finalParticipants.map(member=>{return member+ 'joined in the contest:';})
+console.log(announcements)
+
+*/
